0fae1bbe307ca7570a9a1742d8c98e674267d5f5
Vishnupriyaaa <madireddyvishnupriya@gmail.com>
2020-08-02 11:46:22 +0530

    hh

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './components/helpers/configureStore';
-import { BrowserRouter } from 'react-router-dom';
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
